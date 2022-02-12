9c5a46f6d75b7693200e4825d2e2a0b117c72d23
irinap20 <91842048+irinap20@users.noreply.github.com>
2022-02-11 23:50:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+document.onscroll = function() {
+    const specs = document.querySelector('#specs');
+    const nav = document.querySelector('nav');
+    
+    if(specs.getBoundingClientRect().top <= 0) { // if the distance of the 'specs' section to the browser top is smaller than 0
+      nav.classList.add('dark'); // add dark font color
+      if(specs.getBoundingClientRect().top >=0){
+        nav.classList.remove('dark'); // remove dark  font color
+    } 
+    } else {
+      nav.classList.remove('dark'); // remove dark  font color
+    }
+  } 
+
+
+  function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+    
+  }
+
+  
