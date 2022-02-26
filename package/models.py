05fdc6504964075ece91dac97a9c57d6a0efb556
yunip20 <91842048+yunip20@users.noreply.github.com>
2022-02-25 19:49:51 -0500

    Add files via upload

---
+++ package/models.py
@@ -0,0 +1,9 @@
+from flask import session
+from flask_login import UserMixin 
+from sqlalchemy.ext.automap import automap_base 
+
+# automap base
+Base = automap_base()   
+
+class Calc_user(Base, UserMixin):
+    __tablename__ = 'Calc_user' 
