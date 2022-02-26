a39e91df181e82c9f716f6bff69bf9b5f5fe3624
yunip20 <91842048+yunip20@users.noreply.github.com>
2022-02-25 19:28:40 -0500

    Delete mazegame directory

--- mazegame/src/Comaprator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-// Compares the weights of Edges
-class WeightCompare implements Comparator<Edge> { 
-  public int compare(Edge e1, Edge e2) {
-    return e1.weight - e2.weight;
-  }
-}
